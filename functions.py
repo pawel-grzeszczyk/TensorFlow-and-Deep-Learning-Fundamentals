476a1a7de76912009c6f733724a0282a4cd87936
pawel_grzeszczyk <p.grzeszczyk00@gmail.com>
2022-10-29 16:07:39 +0200

    03_file and functions update

--- functions.py
+++ functions.py
@@ -24,3 +24,33 @@ def view_random_image(target_dir, target_class):
     print(f'Image shape: {img.shape}') 
 
     return img
+
+
+def plot_loss_curves(history):
+    '''
+    Returns sepatrate loss curves for training and validation metrics
+    '''
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    epochs = range(len(history.history['loss'])) # how many epochs did we run for
+
+    # Plot loss
+    plt.figure(figsize=(16, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='training_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('loss')
+    plt.xlabel('epochs')
+    plt.legend()
+
+    # Plot loss
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='training_accuracy')
+    plt.plot(epochs, val_accuracy, label='val_accuracy')
+    plt.title('accuracy')
+    plt.xlabel('epochs')
+    plt.legend()
