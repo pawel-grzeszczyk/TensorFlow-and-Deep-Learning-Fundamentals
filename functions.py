ca65f2783965d8ec7855e63848219c1bc4bdb7fa
pawel_grzeszczyk <p.grzeszczyk00@gmail.com>
2022-10-31 19:34:25 +0100

    file_04, gitignore and functions update

--- functions.py
+++ functions.py
@@ -1,12 +1,12 @@
-import matplotlib.pyplot as plt
-import random
-import matplotlib.image as mpimg
-import os
-
 def view_random_image(target_dir, target_class):
     '''
     Prints a random image in target_class from target_dir.
     '''
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    import random
+    import os
+
     # Setup the target directory
     target_folder = target_dir + '/' + target_class
 
@@ -26,15 +26,24 @@ def view_random_image(target_dir, target_class):
     return img
 
 
-def plot_loss_curves(history):
+def plot_loss_curves(history, metric_name='accuracy'):
     '''
-    Returns sepatrate loss curves for training and validation metrics
+    Returns sepatrate loss curves for training and validation metrics.
+
+    Args:
+        history: TensorFlow history object.
+        metric_name: metric used during fitting a model, default: accuracy
+
+    Returns:
+        Plots of training/validation loss and metric
     '''
+    import matplotlib.pyplot as plt
+
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
-    accuracy = history.history['accuracy']
-    val_accuracy = history.history['val_accuracy']
+    metric = history.history[metric_name]
+    val_metric = history.history['val_'+metric_name]
 
     epochs = range(len(history.history['loss'])) # how many epochs did we run for
 
@@ -49,8 +58,82 @@ def plot_loss_curves(history):
 
     # Plot loss
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, accuracy, label='training_accuracy')
-    plt.plot(epochs, val_accuracy, label='val_accuracy')
-    plt.title('accuracy')
+    plt.plot(epochs, metric, label='training_'+metric_name)
+    plt.plot(epochs, val_metric, label='val_'+metric_name)
+    plt.title(metric_name)
     plt.xlabel('epochs')
     plt.legend()
+
+
+def create_tensorboad_callback(dir_name, experiment_name):
+    '''
+    Creates a TensorBoard callback.
+
+    Args:
+        dir_name (str): A directory where TensorBoard output will be saved.
+        experiment_name (str): name of the experiment.
+
+    Returns:
+        TensorBoard callback stored in a dir_name directory.
+    '''
+    import datetime
+    import tensorflow as tf
+
+    # Create a log directory
+    log_dir = dir_name + '/' + experiment_name + '/' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    # Set the TensorBoard callback
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+
+    # Print output
+    print(f'Saving TensorBoad log files to {log_dir}')
+
+    return tensorboard_callback
+
+
+def create_model(model_url, trainable=False, layer_name='feature_extraction_layer', 
+                 image_shape=(224, 224), clasification_type='multiclass', num_classes=10):
+    '''
+    Takes a TensorFlow Hub URL and creates a Keras Sequential model with it.
+
+    Args:
+        model_url (str): A TensorFlow Hub feature extraction URL.
+        trainable (bool): Do you want to reset already learned features?
+            Default False.
+        layer_name (str): name of the feature_extractor layer,
+            default: "feature_extraction_layer"
+        image_shape (int, int): shape of the image (height, width),
+            default: (224, 224)
+        classification_type (str): "multiclass" for multiclass classification,
+            "binary" for binary classification
+        num_classes (int): Number of output neurons in the output layer,
+            should be equal to number of target classes, default 10.
+
+    Returns:
+        An uncompiled Keras Sequential model with model_url as feature extractor
+        layer and Dense output layer with num_classes output neurons.
+    '''
+    import tensorflow as tf
+    import tensorflow_hub as hub
+
+    # Download the pretrained model and save it as a Keras layer
+    feature_extractor_layer = hub.KerasLayer(model_url,
+                                             trainable=trainable, # freeze the already learned patterns
+                                             name=layer_name, 
+                                             input_shape=image_shape+(3,)) 
+
+    # Set output layer activation function based on clasification_type:
+    if clasification_type == 'multiclass': 
+        activation_function = 'softmax' 
+    elif clasification_type == 'binary':
+        activation_function = 'sigmoid' 
+    else:
+        raise ValueError('clasification_function must be "multiclass" or "binary"')
+
+    # Create a model
+    model = tf.keras.models.Sequential([
+        feature_extractor_layer,
+        tf.keras.layers.Dense(num_classes, activation=activation_function)
+    ])
+
+    return model
